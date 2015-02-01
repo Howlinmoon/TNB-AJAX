6423f56673fa08616132647348bc6fb8361d62ad
Jim Veneskey <github@strifecrafter.com>
2015-01-31 20:37:24 -0500

    Tutorial #05, finished creating the AJAX PHP responder

--- Tutorial-03/foodstore.php
+++ Tutorial-03/foodstore.php
@@ -3,9 +3,18 @@ header("Content-type: text/xml');
 echo '<?xml  version="1.0" encoding="UTF-8" standalone="yes" ?>	
 
 echo '<response>';
-
-
-
+	# $food is what we are passed by AJAX
+	$food = $_GET['food'];
+	# this is our array of food items we have in stock
+	$foodArray = array('tuna', 'bacon', 'beef', 'loaf', 'ham',' spaghetti');
+	# checking to see if the requested food item matches the contents of our array
+	if (in_array($food, $foodArray)) {
+		echo 'We do have ' . $food. '!';
+	elseif($food == "") {
+		echo 'Echo a food you putz';
+	} else {
+		echo 'Sorry punk we don't see no '.$food.'!';
+	}
 
 echo '</response>';
 
