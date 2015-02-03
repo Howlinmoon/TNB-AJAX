ab00ca94927daa944ef0080a44e315956ab71a5f
Jim Veneskey <github@strifecrafter.com>
2015-02-02 20:20:31 -0500

    Tutorial #13, starting a new example

---
+++ Tutorial-13/movies.js
@@ -0,0 +1,11 @@
+function createList() {
+	var s;
+	s = "<ul>"
+		+ "<li>Colossus</li>"
+		+ "<li>When Worlds Collide</li>"
+		+ "<li>Day The Earth Stood Still</li>"
+		+ "</ul>";
+	var divMovies = document.getElementById("divMovies");
+	divMovies.innerHTML = s;
+	
+}
