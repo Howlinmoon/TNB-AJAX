6b267b4dd249b00cfd55bea9cc10425cec186a08
Jim Veneskey <github@strifecrafter.com>
2015-02-02 20:35:19 -0500

    Tutorial #16, continuing the example, creatig nodes by hand

--- Tutorial-15/movies.js
+++ Tutorial-15/movies.js
@@ -3,5 +3,20 @@ function gametime()  {
 	var title = document.createTextNode("Here are some things!");
 	var list = document.createElement("ul");
 	
+	var item1 = document.createElement("li");
+	var text1 = document.createTextNode("Radio Shack Model 1");
+	item1.appendChild(text1);
 	
+	var item2 = document.createElement("li");
+	var text2 = document.createTextNode("Radio Shack Color Computer");
+	item2.appendChild(text2);
+
+	var item3 = document.createElement("li");
+	var text3 = document.createTextNode("Radio Shack Model 4P");
+	item3.appendChild(text3);
+	
+	list.appendChild(item1);
+	list.appendChild(item2);
+	list.appendChild(item3);
+
 }
