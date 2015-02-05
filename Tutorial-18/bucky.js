e0c51af8536f90d5945567d8014c8f380ad4f126
Jim Veneskey <github@strifecrafter.com>
2015-02-04 20:41:58 -0500

    Tutorial #20, sweet, sweet dynamic HTML

---
+++ Tutorial-18/bucky.js
@@ -0,0 +1,24 @@
+function dude () {
+	var table = document.getElementById("table");
+	var th = document.getElementById("tableHead");
+	var item1 = document.getElementById("item1");
+	var item2 = document.getElementById("item2");
+	
+	table.className = "dudeTable";
+	th.className = "dudeHead";
+	item1.className = "dudeItem";
+	item2.className = "dudeItem";
+}
+
+function chick () {
+	var table = document.getElementById("table");
+	var th = document.getElementById("tableHead");
+	var item1 = document.getElementById("item1");
+	var item2 = document.getElementById("item2");
+	
+	table.className = "chickTable";
+	th.className = "chickHead";
+	item1.className = "chickItem";
+	item2.className = "chickItem";
+	
+}
