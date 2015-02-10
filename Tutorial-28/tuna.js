c44aa806835bcc358079c96fe1f5ad8de1bbfd30
Jim Veneskey <github@strifecrafter.com>
2015-02-10 06:36:16 -0500

    Tutorial #30 - adding the js file

---
+++ Tutorial-28/tuna.js
@@ -0,0 +1,25 @@
+var xmlHttp = createXmlHttpRequestObject();
+
+function createXmlHttpRequestObject() {
+	var xmlHttp;
+	if (window.XMLHttpRequest) {
+		xmlHttp = new XMLHttpRequest();
+	} else {
+		xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	return xmlHttp;
+}
+
+// called on load
+function process() {
+	if (xmlHttp) {
+		try {
+			xmlHttp.open("GET", "tuna.xml", true);
+			xmlHttp.onreadystatechange = handleStateChange;
+			xmlHttp.send(null);
+		} catch (e) {
+			alert(e.toString());
+		}
+	}
+}
+
